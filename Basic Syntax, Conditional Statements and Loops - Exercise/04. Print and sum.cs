5ad4d494cb5b7dcf710b0e8c6ef84b288f2ac252
Winchestur <deividyordanov@abv.bg>
2024-04-06 12:39:11 +0300

    Exercise HomeWork

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and sum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int startNum = int.Parse(Console.ReadLine());
+        int EndNum = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = startNum; i <= EndNum; i++)
+        {
+            sum += i;
+            Console.Write($"{i} ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Sum: {sum}");
+
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/06. Strong number.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int takeNumber = number;
+        int sum = 1;
+        int totalResult = 0;
+
+        while (takeNumber > 0)
+        {
+            int takeLastNum = takeNumber % 10;
+            takeNumber /= 10;
+
+            for (int i = takeLastNum; i >= 1; i--)
+            {
+                sum *= i;
+            }
+            totalResult += sum;
+            sum = 1;
+        }
+
+        if (totalResult == number)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+
+    }
+}
