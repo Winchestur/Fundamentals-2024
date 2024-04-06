78971fd8fdcae814ec71ca6c7c949abc50192b0d
Winchestur <deividyordanov@abv.bg>
2024-04-06 12:05:06 +0300

    Basic syntax homework completed

---
+++ Basic Syntax, Conditional Statements and Loops/03. Passed or Failed.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double grade = double.Parse(Console.ReadLine());
+
+        if (grade >= 3.00)
+        {
+            Console.WriteLine($"Passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Failed!");
+        }
+    }
+}
---
+++ Basic Syntax, Conditional Statements and Loops/07. Theatre Promotion.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string typeOfDay = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        int price = 0;
+
+        switch (typeOfDay)
+        {
+            case "Weekday":
+
+                if (age >= 0 && age <= 18)
+                {
+                    price = 12;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 18;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 12;
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+
+                break;
+            case "Weekend":
+
+                if (age >= 0 && age <= 18)
+                {
+                    price = 15;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 20;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 15;
+                }
+                else
+                {
+                    Console.WriteLine($"Error!");
+                }
+
+                break;
+            case "Holiday":
+
+                if (age >= 0 && age <= 18)
+                {
+                    price = 5;
+                }
+                else if (age > 18 && age <= 64)
+                {
+                    price = 12;
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    price = 10;
+                }
+                else
+                {
+                    Console.WriteLine($"Error!");
+                }
+
+                break;
+        }
+
+        if (age >= 0 && age <= 122)
+        {
+            Console.WriteLine($"{price}$");
+        }
+    }
+}
