2e26a87d0203d0b56f78447d3d861b0571f24ad9
Winchestur <deividyordanov@abv.bg>
2024-04-10 22:16:53 +0300

    Methods - Lab HomeWork

---
+++ 09.Methods - Lab/04. Printing Triangle.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        printingTriangle(n);
+    }
+
+    private static void printingTriangle(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write($"{j} ");
+            }
+            Console.WriteLine();
+        }
+
+        int counter = 1;
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                Console.Write($"{counter} ");
+                counter++;
+            }
+            Console.WriteLine();
+            counter = 1;
+        }
+
+    }
+}
---
+++ 09.Methods - Lab/07. Repeat String.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+
+        string newText = repeater(text, n);
+
+        Console.WriteLine(newText);
+    }
+
+    private static string repeater(string text, int n)
+    {
+        string newText = "";
+
+        for (int i = 0; i < n; i++)
+        {
+            newText += text;
+        }
+        return newText;
+    }
+}
