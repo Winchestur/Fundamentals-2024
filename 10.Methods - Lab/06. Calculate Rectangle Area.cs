8f383c1e49fef53807174136ddf908ff31047d9c
Winchestur <deividyordanov@abv.bg>
2024-04-10 22:19:23 +0300

    Methods Lab - HomeWork

---
+++ 10.Methods - Lab/06. Calculate Rectangle Area.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int firstNum = int.Parse(Console.ReadLine());
+        int secondNum = int.Parse(Console.ReadLine());
+
+        int result = area(firstNum, secondNum);
+
+        Console.WriteLine(result);
+    }
+
+    private static int area(int firstNum, int secondNum)
+    {
+
+        return firstNum * secondNum;
+    }
+}
---
+++ 10.Methods - Lab/08. Math Power.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double Base = double.Parse(Console.ReadLine());
+        double power = double.Parse(Console.ReadLine());
+
+        double result = mathPower(Base, power);
+        Console.WriteLine(result);
+    }
+
+    private static double mathPower(double Base, double power)
+    {
+        double result = Math.Pow(Base, power);
+
+        return result;
+    }
+}
